4bc0714f5d22843f7e89c475bd9e40057999b9eb
Senuda Ratnayake <tdr5250@psu.edu>
2022-04-10 11:55:40 -0400

    completed the login / logout function

--- ViewControllers/UserProfileViewController.swift
+++ ViewControllers/UserProfileViewController.swift
@@ -6,16 +6,41 @@
 //
 
 import UIKit
+import Parse
 
 class UserProfileViewController: UIViewController {
-
+    @IBOutlet weak var userBarButton: UIBarButtonItem!
+    @IBOutlet weak var logoutBarButton: UIBarButtonItem!
+    
+    let user = PFUser.current()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        //changing the UserActive bar button item to the current logged in user.
+        userBarButton.title = user?.username
+        userBarButton.setTitleTextAttributes([
+            NSAttributedString.Key.font: UIFont(name: "Noteworthy-Bold", size: 26.0)!,
+            NSAttributedString.Key.foregroundColor: UIColor.systemBackground],
+            for: .normal)
+        
+        //changing the logoutBarButton item apperence.
+        logoutBarButton.tintColor = UIColor.systemBackground
     }
     
-
+    @IBAction func onLogoutButton(_ sender: Any) {
+        PFUser.logOut()
+        
+        let main = UIStoryboard(name: "Main", bundle: nil)
+        
+        let loginViewController = main.instantiateViewController(withIdentifier: "LoginViewController")
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,let delegate = windowScene.delegate as? SceneDelegate else { return }
+        
+        delegate.window?.rootViewController = loginViewController
+    }
+    
     /*
     // MARK: - Navigation
 
